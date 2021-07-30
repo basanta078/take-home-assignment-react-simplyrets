eb17cc22744cb9b6d462ba3cb173a8513b4b1195
droidwar <76974855+droidwar@users.noreply.github.com>
2021-07-30 10:55:39 -0400

    added header component todo: make it sticky

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,13 @@
 
 import heartFill from './assets/heart-fill.svg';
 import heartStroke from './assets/heart-stroke.svg';
+import { Header } from './components/header/Header';
 
 function App() {
   return (
+    <><Header
+        title="Here is the title"
+      />
     <div
       style={{
         maxWidth: '70%',
@@ -16,25 +20,13 @@ function App() {
         background: '#eee',
       }}
     >
-      <h1>Side React Take-home Assignment</h1>
-      <p>
-        Welcome to the Side React take-home assignment. This boilerplate is
-        intended to help get you started. It's already setup with the fonts and
-        SVG assets you will need to complete your assignment.
-      </p>
-      <p>
-        <em>
-          Be sure to read through the INSTRUCTIONS.md content before beginning.
-          Good luck!
-        </em>
-      </p>
-      <div>
-        <div>Here are the SVG assets you will need.</div>
-        <img src={heartFill} className="App-logo" alt="favorite icon" />
-        <img src={heartStroke} className="App-logo" alt="unfavorite icon" />
-      </div>
+      Main Content
     </div>
+    </>
   );
 }
 
 export default App;
+
+//<img src={heartFill} className="App-logo" alt="favorite icon" />
+//<img src={heartStroke} className="App-logo" alt="unfavorite icon" />
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,17 @@
+import css from './header.module.scss';
+
+type HeaderProps = {
+    title: string;
+}
+
+export function Header({title}: HeaderProps): JSX.Element{
+    return (
+        <div className={css.header} role="heading" aria-level={1}>
+            <div className={css.container}>
+                <text>
+                    {title}
+                </text>
+            </div>
+        </div>
+    )
+}
