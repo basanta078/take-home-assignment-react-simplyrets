55b9fb8f181f601986b33f4a3a3cb90db23a37a7
droidwar <76974855+droidwar@users.noreply.github.com>
2021-07-30 11:09:37 -0400

    todo property, move to api first

---
+++ src/components/Property/Property.tsx
@@ -0,0 +1,13 @@
+type PropertyProps = {
+    bedrooms: number;
+    bathsFull: number;
+    bathsHalf: number;
+    area: number;
+    listPrice: number;
+    address: string;
+    listDate: Date;
+    photos: string[];
+}
+export function Property(props: PropertyProps){
+
+}
